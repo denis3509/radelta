2a85feee3f600f2d75cd0e9a36edf918a754f8f6
denis <denis3509@gmail.com>
2023-10-24 10:37:58 +0300

    task comments

--- delivery/tasks.py
+++ delivery/tasks.py
@@ -9,23 +9,24 @@ logger = get_logger()
 
 @celery_app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
+    """ initialize periodic tasks"""
     sender.add_periodic_task(
         crontab(minute="*/5"),
         update_delivery_costs.s(),
         queue=Queue.MAIN,
-        periodic_task_name=""
+        periodic_task_name="update_delivery_costs"
     )
 
 
 @celery_app.task()
 def update_delivery_costs():
+    """task: calculates delivery costs for packages that wasn't processed"""
     srv.update_delivery_costs()
-
 
 
 @celery_app.task()
 def update_pkg_delivery_cost(pkg_id: int):
-
+    """task: update delivery cost for a package"""
     pkg = models.Package.objects.get(id=pkg_id)
 
     srv.update_pkg_delivery_cost(pkg)
@@ -33,5 +34,6 @@ def update_pkg_delivery_cost(pkg_id: int):
 
 @celery_app.task()
 def register_package(data: dict, session_id: str):
+    """task: register package and update cost"""
     pkg = srv.register_package(data, session_id)
     update_pkg_delivery_cost.delay(pkg.id)
